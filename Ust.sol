386ec524509df0f575618a83428801f5ab0c5ba2
Daniel Hajný <52800583+DvorakuvPadawan@users.noreply.github.com>
2023-04-07 13:41:29 +0200

    Create Ust.sol

---
+++ Ust.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Ust is ERC20, Ownable, ERC20Burnable {
+    constructor() ERC20("Ust token", "UST") {
+    }
+    function mint(address reciever, uint256 amount) public onlyOwner {
+        _mint(reciever, amount);
+    }
+    function _burn(uint256 amount) public onlyOwner {
+        burn(amount);
+    }
+}
